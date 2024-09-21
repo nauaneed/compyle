49d6404c959e53911220cd05c07dbd5405a1a4bb
Navaneet Villodi <11260095+nauaneed@users.noreply.github.com>
2024-09-21 15:37:04 +0530

    please make some sense

--- compyle/tests/test_ext_module.py
+++ compyle/tests/test_ext_module.py
@@ -240,7 +240,7 @@ class TestExtModule(TestCase):
 
         # Then
         # The shutil.copy should have been run only once.
-        warnings.warn(f'{result=}\n{sum(result)=}')
+        warnings.warn(f'{result=}\n{sum(result)=}\n{type(sum(result))}')
         self.assertEqual(sum(result), 1)
 
 
